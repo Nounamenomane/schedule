d2ce9389ab6ca2d86ac9a5aab89a7ab6cde7d904
nounamenomane <nounamenomane@gmail.com>
2023-09-20 19:54:52 +0600

    fix bag

--- src/lessons/Lesson.jsx
+++ src/lessons/Lesson.jsx
@@ -1,4 +1,5 @@
 import Time from "../time/Time";
+import WensdayTime from "../time/WensdayTime";
 import css from "./Lesson.module.css";
 function Lesson(props) {
   return (
@@ -7,13 +8,21 @@ function Lesson(props) {
         <h2>{props.day}</h2>
       </div>
       <div className={css.lessons}>
-        <Time />
+        {props.day === "Понедельник" ||
+        props.day === "Вторник" ||
+        props.day === "Четверг" ||
+        props.day === "Пятница" ? (
+          <Time timeData={props.timeData} />
+        ) : null}
+        {props.day === "Среда" ? (
+          <WensdayTime timeData={props.timeData} />
+        ) : null}
         <div className={css.lesson}>
           <h3>{props.lesson1}</h3>
-          <h3>{props.lesson2}</h3>
-          <h3>{props.lesson3}</h3>
+          <h3 style={props.style1}>{props.lesson2}</h3>
+          <h3 style={props.style2}>{props.lesson3}</h3>
           <h3>{props.lesson4}</h3>
-          <h3>{props.lesson5}</h3>
+          <h3 style={props.style3}>{props.lesson5}</h3>
           <h3>{props.lesson6}</h3>
           <h3>{props.lesson7}</h3>
           <h3>{props.lesson8}</h3>
--- src/time/WensdayTime.jsx
+++ src/time/WensdayTime.jsx
@@ -1,5 +1,6 @@
 import css from "./Time.module.css";
-function Time(props) {
+
+function WensdayTime(props) {
   const time = (number, time) => {
     return (
       <div className={css.time_number}>
@@ -11,16 +12,16 @@ function Time(props) {
 
   return (
     <div className={css.times}>
-      {time(1, "7.40 – 8.25")}
-      {time(2, "8.30 – 9.15")}
-      {time(3, "9.20 – 10.05")}
-      {time(4, "10.15 – 11.00")}
-      {time(5, "11.05 – 11.50")}
+      {time(1, "7.40 – 08.10")}
+      {time(2, "08.15 – 08.55")}
+      {time(3, "09.00 – 09.40")}
+      {time(4, "09.45 – 10.25")}
+      {time(5, "10.30 – 11.10")}
       {time(6, "11.55 – 12.40")}
-      {time(7, "12.45 – 13.30")}
-      {time(8, "13.40 – 14.25")}
+      {time(7, "12.00 – 12.40")}
+      {time(8, "12.45 – 13.25")}
     </div>
   );
 }
 
-export default Time;
+export default WensdayTime;
